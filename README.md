960d86cc9385a2bccc46b3d1ea92fd9f831b0c05
alyssahellrung <alyssahellrung@gmail.com>
2020-10-12 10:35:02 -0700

    html, css, initial JS finished

---
+++ README.md
@@ -0,0 +1,39 @@
+# ADVENTURE MALL (WITH ZOMBIES)
+
+## Description  
+You're at the raddest mall in town and feeling really cool. Shop around at all of the most popular stores, but watch out for the zombie dudes because they are grody to the max!
+
+## Table of Contents
+
+* [Technologies Used](#technologies-used)
+
+* [How to Use](#how-to-use)
+
+* [How I Made This](#how-i-made-this)
+
+* [Deployed Application](#link-to-deployed-application)
+
+* [GitHub Repo](#link-to-github-repo)
+
+* [Future Development Ideas](#future-development-ideas)
+
+## Technologies Used  
+Built in Visual Studio with        
+HTML     
+CSS    
+Javascript      
+
+## How to Use  
+Coming soon.
+
+## How I Made This  
+This began with an adventure game tutorial by Web Dev Simplified on YouTube (https://www.youtube.com/watch?v=R1S_NhKkvGA). I completed the tutorial, then went through and changed the story from a traditional adventure story (swords, castles, merchants, etc.) to a mall story. I also changed up the styling a bit to reflect a more 80s aesthetic.
+
+## Link to Deployed Application
+Coming soon.
+
+## Link to GitHub Repo    
+(https://github.com/alyssahellrung/adventureMallWithZombies)
+
+## Future Development Ideas
+I would love to turn this into a more robust game with various playable characters and the ability to have more items in the player's inventory. Pictures to accompany each question would be cool too.
---
+++ game.js
@@ -0,0 +1,110 @@
+const textElement = document.getElementById("text")
+const optionButtonsElement = document.getElementById("option-buttons")
+
+let state = {}
+
+function startGame() {
+  state = {}
+  showTextNode(1)
+}
+
+function showTextNode(textNodeIndex) {
+  const textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
+  textElement.innerText = textNode.text
+  while (optionButtonsElement.firstChild) {
+    optionButtonsElement.removeChild(optionButtonsElement.firstChild)
+  }
+
+  textNode.options.forEach(option => {
+    if (showOption(option)) {
+      const button = document.createElement("button")
+      button.innerText = option.text
+      button.classList.add("btn")
+      button.addEventListener("click", () => selectOption(option))
+      optionButtonsElement.appendChild(button)
+    }
+  })
+}
+
+function showOption(option) {
+  return option.requiredState == null || option.requiredState(state)
+}
+
+function selectOption(option) {
+  const nextTextNodeId = option.nextText
+  if (nextTextNodeId <= 0) {
+    return startGame()
+  }
+  state = Object.assign(state, option.setState)
+  showTextNode(nextTextNodeId)
+}
+
+const textNodes = [
+  {
+    id: 1,
+    text: "You wake up in a strange place and you see a jar of blue goo near you.",
+    options: [
+      {
+        text: "Take goo",
+        setState: { blueGoo: true },
+        nextText: 2
+      },
+      {
+        text: "Leave the goo",
+        nextText: 2
+      }
+    ]
+  },
+  { 
+    id: 2,
+    text: "You venture forth in search of answers to where you are when you come across a merchant.",
+    options: [
+      {
+        text: "Trade the goo for a sword",
+        requiredState: (currentState) => currentState.blueGoo,
+        setState: { blueGoo: false, sword: true },
+        nextText: 3
+      },
+      {
+        text: "Trade the goo for a shield",
+        requiredState: (currentState) => currentState.blueGoo,
+        setState: { blueGoo: false, shield: true },
+        nextText: 3
+      },
+      {
+        text: "Ignore the merchant",
+        nextText: 3
+      }
+    ]
+  },
+  {
+    id: 3,
+    text: "After leaving the merchant, you start to feel tired and stumble upon a small town next to a dangerous looking castle.",
+    options: [
+      {
+        text: "Explore the castle",
+        nextText: 4
+      },
+      {
+        text: "Find a room in the town to sleep in",
+        nextText: 5
+      },
+      {
+        text: "Find some hay in a stable to sleep in",
+        nextText: 6
+      }
+    ]
+  },
+  {
+    id: 4,
+    text: "You are so tired that you fall asleep while exploring the castle and you are killed by a terrible monster in your sleep.",
+    options: [
+      {
+        text: "Restart",
+        nextText: -1
+      }
+    ]
+  }
+]
+
+startGame()
